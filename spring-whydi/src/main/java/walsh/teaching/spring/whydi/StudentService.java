194b5e136a8213ba29d753058c5520ec484c37db
walshzhang <walsh.zhang@hotmail.com>
2015-07-29 23:12:13 +0800

    construct project structure. initialize spring-whydi project

--- spring-whydi/src/main/java/walsh/teaching/spring/whydi/StudentService.java
+++ spring-whydi/src/main/java/walsh/teaching/spring/whydi/StudentService.java
@@ -1,9 +1,15 @@
 package walsh.teaching.spring.whydi;
 
+import java.util.List;
+
 public class StudentService {
     private StudentDao studentDao;
 
     public StudentService() {
         this.studentDao = new FileBasedStudentDao("students.txt");
+    }
+
+    public List<Student> findByName(String name) {
+        return studentDao.findByNames(name);
     }
 }
